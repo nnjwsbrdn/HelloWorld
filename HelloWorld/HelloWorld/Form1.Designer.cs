4c645647223809518618994276d1a712dcae25bd
nnjwsbrdn <116859559+nnjwsbrdn@users.noreply.github.com>
2022-11-25 10:44:38 +0800

    Add files via upload

---
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloLabel = new System.Windows.Forms.Label();
+            this.Change = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HelloLabel
+            // 
+            this.HelloLabel.AutoSize = true;
+            this.HelloLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelloLabel.Location = new System.Drawing.Point(43, 24);
+            this.HelloLabel.Name = "HelloLabel";
+            this.HelloLabel.Size = new System.Drawing.Size(41, 15);
+            this.HelloLabel.TabIndex = 0;
+            this.HelloLabel.Text = "Hello";
+            this.HelloLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Change
+            // 
+            this.Change.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Change.Location = new System.Drawing.Point(111, 19);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(79, 31);
+            this.Change.TabIndex = 1;
+            this.Change.Text = "Change";
+            this.Change.UseVisualStyleBackColor = true;
+            this.Change.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 555);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.HelloLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloLabel;
+        private System.Windows.Forms.Button Change;
+    }
+}
+
---
+++ HelloWorld/HelloWorld/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (HelloLabel.Text == "Hello" )
+            {
+                HelloLabel.Text = "Salam";
+            }
+            else if (HelloLabel.Text == "Salam")
+                        {
+                HelloLabel.Text = "Kabare";
+            }
+            else 
+            {
+                HelloLabel.Text = "Hello";
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
